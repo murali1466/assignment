fc1c2f5cc1a2a7e1c2fe55098fcf32458de713b5
murali1466 <muralikrishnatangella1466@gmail.com>
2024-02-27 22:06:24 +0530

    s

--- sricpt.js
+++ sricpt.js
@@ -1,15 +1,17 @@
 
 const cards = document.querySelectorAll('.box-container');
 const sizes = document.querySelectorAll('.show');
-
+const totalEle = document.getElementById('totalId')
 cards.forEach(card => {
   card.addEventListener('click', () => {
     const radio = card.querySelector('input[type="radio"]');
     const showEle = card.querySelector('.show');
+    const value = radio.value;
     radio.checked = true;
     cards.forEach(c => c.classList.remove('selected'));
     sizes.forEach(s => s.classList.remove('visible'));
     card.classList.add('selected');
     showEle.classList.add('visible');
+    totalEle.innerText = '$' + value + '.00 USD';
   });
 });
